0a083e0f042157eeb4e22254fa9c00ad3a886756
Volodka2005 <144377945+Volodka2005@users.noreply.github.com>
2024-10-07 21:03:35 +0300

    Create registration and authorization

--- api/index.js
+++ api/index.js
@@ -2,10 +2,14 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const User = require('./models/User')
+const bcrypt = require('bcrypt');
 const app = express();
+const jwt = require('jsonwebtoken')
 
+const salt = bcrypt.genSaltSync(10);
+const secret = 'fdpg[dgldknklamsdsmfk';
 
-app.use(cors());
+app.use(cors({credentials:true,origin:'http://localhost:3000'}));
 app.use(express.json());
 
 mongoose.connect('mongodb+srv://vovasur27:volodka2005sch@cluster0.pvokg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
@@ -13,19 +17,37 @@ mongoose.connect('mongodb+srv://vovasur27:volodka2005sch@cluster0.pvokg.mongodb.
 app.post('/register', async (req, res) => {
     const {username, password} = req.body;
     try {
-        const userDoc = await User.create({username, password})
-        res.json({userDoc});
+        const userDoc = await User.create({
+            username, 
+            password:bcrypt.hashSync(password,salt)
+        })
+        res.json(userDoc);
     } catch (e){ 
+        console.log(e);
         res.status(400).json(e);
 
     }
 });
 
-
+app.post('/login', async (req,res) => {
+    const {username,password} = req.body;
+    const userDoc = await User.findOne({username})
+    const passOk = bcrypt.compareSync(password, userDoc.password);
+    if (passOk) {
+        //logg
+        jwt.sign({username, id:userDoc._id}, secret, {}, (err, token) => {
+            if (err) throw err;
+            res.cookie('token', token).json('ok');
+        })
+        //res.json();
+    } else {
+        res.status(400).json('Wrong credentials')
+    }
+})
 
 app.listen(4000);
 
 //volodka2005sch
-//vovasur27
+
 
 //
